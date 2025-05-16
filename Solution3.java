5a489e7e583cea271b489178a6e06e20396fc455
VladislavFomin <vladacs03@gmail.com>
2025-05-16 07:08:41 +0500

    Add files via upload

---
+++ Solution3.java
@@ -0,0 +1,33 @@
+package Theme3;
+
+class Solution3 {
+    public Stack<Integer> stac;
+    public Stack<Integer> minStac;
+
+    public MinStack() {
+        stac = new Stack<>();
+        minStac = new Stack<>();
+    }
+
+    public void push(int val) {
+        stac.push(val);
+        if (minStac.isEmpty() || val<=minStac.peek()) {
+            minStac.push(val);
+        }
+    }
+
+    public void pop() {
+        int a = minStac.peek();
+        if (stac.pop()==a) {
+            minStac.pop();
+        }
+    }
+
+    public int top() {
+        return stac.peek();
+    }
+
+    public int getMin() {
+        return minStac.peek();
+    }
+}
